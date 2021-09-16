4198d90b4f581a44ac5e92e6c05d2e41226960c9
Crislander94 <crhighlander94@gmail.com>
2021-09-15 23:41:44 -0500

    xxxx

---
+++ core/routes_repartidor.php
@@ -0,0 +1,19 @@
+<?php
+    function loadController($controller){
+        $name_controller = ucwords($controller).'Controller';   
+        $fileController = 'controller/'.$name_controller.'.php';
+        if(!is_file($fileController)){
+            header('Location: repartidor.php');
+        }
+        //Cargamos el controlador
+        require_once $fileController;
+        $controller = new $name_controller();
+        return $controller;
+    }
+    function loadAction($controller, $action, $conexion, $cod_empresa){
+        if(isset($action) && method_exists($controller, $action)){
+            $controller->$action($conexion,$cod_empresa);
+        }else{
+            header('Location: repartidor.php');
+        }
+    }
---
+++ repartidor.php
@@ -0,0 +1,21 @@
+<?php
+    include_once 'config/settings.php';
+    require_once 'verificar.php';
+    include_once 'partials/cabecera.php';
+    include_once 'partials/menu.php';
+    require_once 'core/routes_admin.php';
+    require_once 'db/conexion.php';
+    $db = new DBClass();
+    $conexion  = $db->getconnection();
+    //Cargar el controlador correspondiente y cargar la accion por GET
+    if(isset($_GET['c'])){
+        $controller = loadController($_GET['c']);
+        if(isset($_GET['a'])){
+            loadAction($controller, $_GET['a'], $conexion, $_SESSION['cod_empresa']);
+        }else{
+            loadAction($controller, ACTION_MAIN, $conexion, $_SESSION['cod_empresa']);
+        }
+    }else{
+        $controller = loadController(CONTROLLER_MAIN_ADMIN);
+        loadAction($controller, ACTION_MAIN, $conexion, $_SESSION['cod_empresa']);
+    }
